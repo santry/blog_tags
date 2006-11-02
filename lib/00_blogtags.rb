b44ecb25edb07715326af04615eaee512828525b
santry <santry@53b936e0-3221-0410-8748-df35822c787b>
2006-11-02 15:52:15 +0000

    Updated README Added some error checking to ensure the "by" attribute is provided correctly, plus some miscellaneous cleanup.
    git-svn-id: http://seansantry.com/svn/radiant/plugins/blogtags/trunk@6 53b936e0-3221-0410-8748-df35822c787b

--- lib/00_blogtags.rb
+++ lib/00_blogtags.rb
@@ -2,8 +2,12 @@ Behavior::Base.define_tags do
   
   tag "next" do |tag|
     current = tag.locals.page
-    by = tag.attr['by'] || 'title'
+    by = (tag.attr['by'] || 'published_at').strip
     
+    unless current.attributes.keys.include?(by)
+      raise TagError.new("`by' attribute of `next' tag must be set to a valid page attribute name")
+    end
+        
     # get the page's siblings, exclude any that have nil 
     # for the sorting attribute, exclude virtual pages,
     # and sort by the chosen attribute
@@ -18,14 +22,19 @@ Behavior::Base.define_tags do
   end
 
   tag "previous" do |tag|
-    current = tag.locals.page
-    by = tag.attr['by'] || 'title'
+    current = tag.locals.page    
+    by = (tag.attr['by'] || 'published_at').strip
+    
+    unless current.attributes.keys.include?(by)
+      raise TagError.new("`by' attribute of `previous' tag must be set to a valid page attribute name")
+    end
+        
     siblings = current.self_and_siblings.delete_if { |s| s.send(by).nil? || s.virtual? }.sort_by { |page| page.attributes[by] }
     index = siblings.index(current)
 
-    # we don't want to wrap around to the last article    
-    if index > 0 then previous = siblings[index - 1] 
-    else previous = nil; end
+    # we don't want to wrap around to the last article  
+    # when we're at the first article  
+    previous = index > 0 ? siblings[index - 1] : nil
     
     if previous
       tag.locals.page = previous
@@ -36,6 +45,5 @@ Behavior::Base.define_tags do
   tag "time_ago_in_words" do |tag|
     ActionView::Base.new.time_ago_in_words(tag.locals.page.published_at || tag.locals.page.created_at)
   end
-  
     
 end
